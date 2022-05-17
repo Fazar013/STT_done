b88342f6e1f96de10b9d1b0ba75c257d414109ab
Doni Mulya Syahputra <donikuh21@gmail.com>
2022-05-17 08:49:03 +0700

    add qr code

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,9 @@ import 'package:flutter/services.dart';
 import 'package:water_drop_nav_bar/water_drop_nav_bar.dart';
 import 'package:alan_voice/alan_voice.dart';
 import 'package:glassmorphism_ui/glassmorphism_ui.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
-void main() {
+Future<void> main() async {
   runApp(const MyApp());
 }
 
@@ -55,59 +56,96 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     /// [AnnotatedRegion<SystemUiOverlayStyle>] only for android black navigation bar. 3 button navigation control (legacy)
-
     return AnnotatedRegion<SystemUiOverlayStyle>(
       value: const SystemUiOverlayStyle(
         systemNavigationBarIconBrightness: Brightness.dark,
       ),
       child: Scaffold(
-       
         body: PageView(
           physics: const NeverScrollableScrollPhysics(),
           controller: pageController,
           children: <Widget>[
             Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Colors.red,
-              Colors.green,
-              Colors.black,
-            ],
-          ),
-        ),
-        child: Center(
-          child: Stack(
-            children: [
-              GlassContainer(
-               height: 300,
-                width: 300,
-               // blur: 1,
-                opacity: 0.1,
-                shadowStrength: 5,
-                child: Center(
-                  child: Text(
-                    "Hello Alan",
-                  ),
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.red,
+                    Colors.green,
+                    Colors.black,
+                  ],
                 ),
               ),
-             
-              
-            ],
-          ),
-        ),
-      ),
+              child: Center(
+                child: Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 60,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          QrImage(
+                            version: QrVersions.auto,
+                            backgroundColor: Colors.white,
+                            foregroundColor: Colors.black,
+                            size: 90,
+                            // embeddedImage: const AssetImage(
+                            //   'assets/icons8-search-50.png',
+                            // ),
+                            // embeddedImageStyle: QrEmbeddedImageStyle(
+                            //   size: const Size(40, 40),
+                            //   color: Colors.white,
+                            // ),
+                            data:
+                                '00020101021126590013ID.CO.BNI.WWW011893600009150211577802096017020480303UMI51450015ID.OR.GPNQR.WWW0215ID10221724287270303UMI5204481453033605802ID5911BW*MOBAZONE6015LABUHANBATU UTA6105214576304B39E',
+                            errorStateBuilder: (cxt, err) {
+                              return const Center(
+                                child: Text(
+                                  "Uh oh! Something went wrong...",
+                                  textAlign: TextAlign.center,
+                                ),
+                              );
+                            },
+                          ),
+                          const Text(
+                            'Hello World AI Apps',
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const GlassContainer(
+                      height: 300,
+                      width: 300,
+                      // blur: 1,
+                      opacity: 0.1,
+                      shadowStrength: 5,
+                      child: Center(
+                        child: Text(
+                          "Hello Alan",
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
             Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisSize: MainAxisSize.max,
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: <Widget>[
-        Image.asset(
-          'asset/ai.jpg', width: 100.0, height: 100.0,
-        ),
-      ],
-    ),
-
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Image.asset(
+                  'asset/ai.jpg',
+                  width: 100,
+                  height: 100,
+                ),
+              ],
+            ),
           ],
         ),
         bottomNavigationBar: ClipRRect(
